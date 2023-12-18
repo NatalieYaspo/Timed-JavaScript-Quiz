985fbdcc85bdf0f840c8ef84852e720975f87567
Traspo <nyaspo8@gmail.com>
2023-12-17 19:56:00 -0500

    adding highscore scection to html and some local storage items in js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,26 +3,28 @@ var questionContainerEl = document.querySelector("#question-container");
 var startButton = document.querySelector("#start-btn");
 var questionEl = document.querySelector("#question");
 var ansButtonEl = document.querySelector("btn");
+var formEl = document.querySelector("#initials-frm");
+var submitButton = document.querySelector("#submit-btn");
+var gameScore = document.querySelector("#gameScore");
+var newScore = document.querySelector("#newScore");
 
 //Question Variable:
 var secondsLeft = 60;
 let currentQuestionIndex
+var scoreCounter = 0;
 
 //ACTIVE Items:
 startButton.addEventListener("click", startGame) //tested with Cosole.log
-
+submitButton.addEventListener("click", logInitialsScore)//NOT TESTED
 
 //functions to play the game
 function startGame() {
     setTimer();
     startButton.classList.add('hide');
     questionContainerEl.classList.remove('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    // shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
     setNextQuestion();
-    
-
-    console.log("test");
 }
 
 function setTimer() {
@@ -37,8 +39,11 @@ function setTimer() {
             gameTimer.textContent = "";
             clearInterval(timeInterval);
             //moves to score screen
+            questionContainerEl.classList.add('hide');
+            showScore();
+            gameScore.classList.remove('hide');
         }
-    }, 1000);
+    }, 500);
 }
 
 
@@ -57,8 +62,22 @@ function selectAnswer() {
 
 }
 
-function logScore() {
+function showScore() {
 
+}
+
+function getHighScores () { //I have no idea if this will work.  I may need to rework.
+    var storedScores = localStorage.getItem("newScore");
+    if (storedScores === null) {
+        highScores = showScore.value
+    } else {
+        highScores = storedScores;
+    }
+}
+
+function logInitialsScore() {
+    scoreCounter.textContent = scoreCounter;
+    localStorage.setItem("newScore", scoreCounter);
 }
 
 function refreshScores() {
@@ -70,10 +89,10 @@ const questions = [
     {
         question: "This is my first question?",
         answers: [
-            { text: "Yes", correct: true},
-            { text: "No", correct: false},
-            { text: "No", correct: false},
-            { text: "No", correct: false},
+            { text: "A: Yes", correct: true},
+            { text: "B: No", correct: false},
+            { text: "C: No", correct: false},
+            { text: "D: No", correct: false},
         ]
     }
 ]

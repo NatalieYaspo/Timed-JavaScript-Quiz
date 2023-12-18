35be6ed805ddf61d4c509fdf29373d95e018f57f
Traspo <nyaspo8@gmail.com>
2023-12-18 09:06:41 -0500

    I have new answers appearing with 1st question

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,7 +2,7 @@ var gameTimer = document.querySelector("#gameTimer");
 var questionContainerEl = document.querySelector("#question-container");
 var startButton = document.querySelector("#start-btn");
 var questionEl = document.querySelector("#question");
-var ansButtonEl = document.querySelector("btn");
+var ansButtonEl = document.querySelector("#answer-btn");
 var formEl = document.querySelector("#initials-frm");
 var submitButton = document.querySelector("#submit-btn");
 var gameScore = document.querySelector("#gameScore");
@@ -20,8 +20,8 @@ submitButton.addEventListener("click", logInitialsScore)//NOT TESTED
 //functions to play the game
 function startGame() {
     setTimer();
-    startButton.classList.add('hide');
-    questionContainerEl.classList.remove('hide');
+    startButton.classList.add("hide");
+    questionContainerEl.classList.remove("hide");
     // shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
     setNextQuestion();
@@ -39,27 +39,66 @@ function setTimer() {
             gameTimer.textContent = "";
             clearInterval(timeInterval);
             //moves to score screen
-            questionContainerEl.classList.add('hide');
+            questionContainerEl.classList.add("hide");
             showScore();
-            gameScore.classList.remove('hide');
+            gameScore.classList.remove("hide");
         }
     }, 500);
 }
 
 
 function setNextQuestion() {
+    resetState()
     showQuestion(questions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
     questionEl.innerText = question.question
     question.answers.forEach(answer => {
-        const button = document.createElement('button');
+        //this is supposed to swap out the button placeholders with buttons for new answers.
+        const button = document.createElement("button");
+        button.innerText = answer.text
+        button.classList.add("btn")
+        ansButtonEl.appendChild(button)
+        button.addEventListener("click", selectAnswer)
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+            // scoreCounter++
+        // } else {
+            // secondsLeft.value - 10
+        }
+        // setNextQuestion();
     })
 }
 
-function selectAnswer() {
+function resetState() {
+    while (ansButtonEl.firstChild) {
+        ansButtonEl.removeChild(ansButtonEl.firstChild)
+    }
+}
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(ansButtonEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+
+}
+
+function setStatusClass (element, correct) { //need to add correct and wrong elements to html?
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass (element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
 }
 
 function showScore() {
@@ -92,7 +131,17 @@ const questions = [
             { text: "A: Yes", correct: true},
             { text: "B: No", correct: false},
             { text: "C: No", correct: false},
-            { text: "D: No", correct: false},
+            { text: "D: No", correct: false}
         ]
-    }
+    }, 
+
+    {
+        question: "This is my 2nd question?",
+        answers: [
+            { text: "1: Yes", correct: true},
+            { text: "2: No", correct: false},
+            { text: "3: No", correct: false},
+            { text: "4: No", correct: false}
+        ]
+    }, 
 ]
